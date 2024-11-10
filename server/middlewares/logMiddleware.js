2621c890514b39001d3201c4d29db548e90c260e
Piyush Kumar Mishra <pm455567@gmail.com>
2024-11-10 20:46:49 +0530

    third commit

---
+++ server/middlewares/logMiddleware.js
@@ -0,0 +1,7 @@
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} request to ${req.url}`);
+    next();
+  };
+  
+  module.exports = logRequest;
+  
